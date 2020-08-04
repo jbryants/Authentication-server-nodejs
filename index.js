c282f29b831b7c1b0bace0f318de5e8a61a77066
jbryants <jbryants19d@gmail.com>
2020-08-04 23:32:29 +0530

    revert change

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // Main starting point of app
 const express = require("express");
 const http = require("http");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-
+const bodyParser = require("body-parser"); // body-parser is used for parsing incoming requests
+const morgan = require("morgan"); // morgan is for logging incoming requests
 const app = express();
+const router = require("./router");
 
 // App setup
+// middlewares in express - to process incoming request
+// app.use() to register middlewares.
+app.use(morgan("combined"));
+app.use(
+  bodyParser.json({
+    type: "*/*",
+  })
+);
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
