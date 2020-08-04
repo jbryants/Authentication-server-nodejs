c5c059757eec9d1421f500ebad9fecf74484accd
jbryants <jbryants19d@gmail.com>
2020-08-04 22:56:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Main starting point of app
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+// App setup
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+
+console.log("Server listening on port: ", port);
