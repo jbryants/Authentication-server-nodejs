c8b16022bc4534d38c3cc700e776605a4468be33
jbryants <jbryants19d@gmail.com>
2020-08-07 15:54:50 +0530

    encrypt password stored in db, plus handle some more edge cases around signin

--- models/user.js
+++ models/user.js
@@ -1,10 +1,37 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 // Define the model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String,
+});
+
+// On Save Hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre("save", function (next) {
+  // get access to the user model
+  const user = this;
+
+  // generate a salt then run callback
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    // hash (encrypt) our password using the salt
+    // then run callback
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create a model class
